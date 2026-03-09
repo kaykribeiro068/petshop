e5103bbee99752082edc35c03a7aa34cfe40ec94
kaykribeiro068 <kaykribeirodeoliveira@gmail.com>
2026-03-09 20:11:04 -0300

    Update teste4.py

--- petshop/teste4.py
+++ petshop/teste4.py
@@ -1,10 +1,12 @@
 pets = []
 
+#cadastrar pet, espécie e cpf do dono
 while True:
         nome = input("Digite o nome do seu pet: ")
         especie = input("Digite a espécie do seu animal: ")
 
         cpf = input("CPF do dono: ")
+       #verificar a quantidade de caracteres do cpf
         while len(cpf) < 11:
            print("CPF deve ter pelo menos 11 caracteres")
            cpf = input("CPF do dono: ")
@@ -17,14 +19,16 @@ while True:
         "cpf_dono": cpf,
         "total_servicos": 0
     }
-
+        #jogar o dicionário pet na lista pets
         pets.append(pet)
 
         print("Cadastro realizado com sucesso :)!!")
         print(pets)
 
+        #continuar o serviço
         continuar = input("Deseja cadastrar outro pet? (s/n): ")
 
         if continuar == "n":
          break
+
 
